51f51c1ff9761fcfc7ae5bd8f0be01d5c82353cb
kenney <kenney@neonics.com>
2013-03-27 05:27:17 +0100

    Fix XSLT for IE 10

--- js/xml.js
+++ js/xml.js
@@ -1,19 +1,32 @@
 function xmlRequest( url )
 {
-	if ( window.XMLHttpRequest )
-	{
-		xhttp=new XMLHttpRequest();
-	}
-	else
-	{
-		xhttp=new ActiveXObject("Microsoft.XMLHTTP");
-	}
-	xhttp.open("GET", url, false);
-	xhttp.send("");
-	ret = xhttp.responseXML;
 
-	//alert( "Retrieved '" + url + "':\n" + ret );
-	return ret;
+	if ( window.ActiveXObject )
+	{
+		//xhttp=new ActiveXObject("Microsoft.XMLHTTP");
+		//xhttp.open("GET", url, false);
+		// must be set after open call; makes transformNode method
+		// available.
+		//xhttp.responseType = 'msxml-document';
+
+		// the below makes the xdoc.tranformNode available.
+
+		var xdoc = new ActiveXObject("MSXML2.DOMDocument");
+		xdoc.async=false;
+		xdoc.load( url );
+		return xdoc;
+	}
+	else if ( window.XMLHttpRequest )
+	{
+		// this is also available in IE, but results in the doc not
+		// having the transformNode method.
+		xhttp=new XMLHttpRequest();
+		xhttp.open("GET", url, false);
+		xhttp.send("");
+		return xhttp.responseXML;
+	}
+	else alert("Incompatible browser");
+
 }
 
 
@@ -29,22 +42,20 @@ function serialize( xml )
 
 function parse( xml )
 {
-	if ( window.DOMParser )
+	if ( window.DOMParser ) // FF & IE
 	{
 		return new DOMParser().parseFromString( xml, "text/xml" );
 	}
-	else if ( window.ActiveXObject )
+	else if ( window.ActiveXObject ) // IE
 	{
 		var xmlDoc = new ActiveXObject( // "MSXML2.DOMDocument.3.0" );//
 			"Microsoft.XMLDOM" );
 		xmlDoc.async=false;
 		xmlDoc.loadXML( xml );
-		return xmlDoc.documentElement;
+		return xmlDoc;//.documentElement;
 	}
 	else
-	{
 		alert( "Cannot find XML Parser");
-	}
 }
 
 
@@ -52,14 +63,10 @@ function transform( xml, xsl )
 {
 	try
 	{
-		// code for IE
-		if (window.ActiveXObject)
-		{
-			return xml.transformNode( xsl );
-		//	document.getElementById("example").innerHTML=ex;
-		}
+
 		// code for Mozilla, Firefox, Opera, etc.
-		else if ( document.implementation && document.implementation.createDocument )
+		//if ( document.implementation && document.implementation.createDocument )
+		if ( typeof(XSLTProcessor) != "undefined" )
 		{
 			// if there's some weird error uncomment this, mozilla may 
 			// enter some xml error code
@@ -68,10 +75,37 @@ function transform( xml, xsl )
 			//return xsltProcessor.transformToFragment( xml, document );
 			return xsltProcessor.transformToDocument( xml, document );
 		}
+
+		// code for IE
+		if ( typeof(xml.transformNode) != "undefined" )
+		{
+			var ret = xml.transformNode( xsl );
+			// result is a string, so return doc
+			return parse( ret );
+		}
+
+		// code for IE
+		if (window.ActiveXObject)
+		{
+			// IE 9+
+			var xslt = new ActiveXObject( "Msxml2.XSLTemplate" );
+			var xmldoc = new ActiveXObject( "Msxml2.DOMDocument" );
+			var xsldoc = new ActiveXObject( "Msxml2.FreeThreadedDOMDocument" );
+			xmldoc.loadXML( xml.xml );
+			xsldoc.loadXML( xsl.xml );
+			xslt.stylesheet = xsldoc;
+
+			var xslproc = xslt.createProcessor();
+			xslproc.input = xmldoc;
+			xslproc.transform();
+			return parse( xslproc.output );
+		}
+
+
 	}
 	catch ( e )
 	{
-		alert("Error: " + e + "\n" + serialize( xsl ) );
+		alert("transform(): Error: " + e + "\n" + e.stack );
 	}
 }
 
