62f75fd96d3c5686f8b2e83214d79d5f8a8c6fd1
kenney <kenney@neonics.com>
2015-01-06 16:40:17 +0100

    js/plot.js: refactor; better extreme calculation
    This plot library allows to overlay several graphs onto one,
    in which case all of them are normalized.
    When showing individual graphs, it might sometimes be better
    to normalize just the single graph for higher resolution.
    This commit is a preparatory step in that direction by decoupling
    the normalization factors and terms and applying them during
    rendering, rather than modifying the original data.

--- js/plot.js
+++ js/plot.js
@@ -12,15 +12,136 @@
  */
 !function($) {
 
-	var Plot = function(element,options) {
-		this.options = options;
-		console.log("Plot constructor");
+	var mousepos = [-1,-1];
+	var globmin=1000000, globmax=-1000000; // XXX TODO contextualize
+
+	$.fn.plot = function(action_or_data, options) {
+		console.log("!!!! plot function",arguments, "(type: ", typeof action_or_data, ")");
+	    return this.each(function() {
+
+			if ( typeof(action_or_data) == 'string' )
+				return $.fn.plot[action_or_data].call( $(this), options );
+
+			if ( typeof(action_or_data) != 'object' )
+			{
+				console.log("plot: unknown type for first argument:", action_or_data);
+				return;
+			}
+
+
+
+			var canvas = this;//document.getElementById('canvas');
+
+			canvas.style.position = 'relative'; // for offset
+			canvas.height = 400; // XXX parameterizable
+			canvas.width = Math.round( canvas.parentNode.clientWidth * 0.98 );
+			var allplotdata = action_or_data;
+			var ctx		= canvas.ctx	= canvas.getContext('2d');
+			var h		= canvas.h		= canvas.height;
+			var w		= canvas.w		= canvas.width;
+			var margin	= canvas.margin	= 20;
+			var offs	= canvas.offs	= margin/2; // x offset
+			allplotdata.global_extremes = allplotdata.global_extremes == undefined ? true : allplotdata.global_extremes;
+
+
+			allplotdata.stats = [];
+			canvas.allplotdata = allplotdata;
+			canvas.options = options;
+
+
+			function normalize( val, min, max, h )
+			{
+				return min + ( val - min ) / ( max - min ) * h;
+			}
+
+			function denormalize( val, min, max, h )
+			{
+				return ( val - min ) / h * ( max - min ) + min;
+			}
+
+			// normalize plotdata
+			for ( var c in allplotdata )
+			{
+				if ( c == 'stats' ) continue;
+				if ( c == 'global_extremes' )
+						continue;
+				var min = 1<<30;
+				var max = 0;
+				for ( var i in allplotdata[c] )
+				{
+					min = Math.min( min, allplotdata[c][i][1] );
+					max = Math.max( max, allplotdata[c][i][1] );
+
+					allplotdata[c][i][2] = allplotdata[c][i][1]; // backup orig
+				}
+				allplotdata.stats[c] = { min:min, max:max };
+				console.log( "extremes for ", c, ": min=", min, " max=", max );
+				if ( 0 )
+			//	if ( ! allplotdata.global_extremes )
+					for ( var i in allplotdata[c] )
+						//allplotdata[c][i][1] = min + ( allplotdata[c][i][1] - min ) / (max-min) * h;
+						allplotdata[c][i][1] = normalize( allplotdata[c][i][1], min, max, h ); // XXX change: min+... => h*min+ ....
+						// REVERSE:
+						// y = min + ( x - min ) / ( max - min ) * h
+						// =>   y - min = ( x - min ) / ( max - min ) * h
+						// => ( y - min ) / h = ( x - min ) / ( max - min )
+						// => ( y - min ) / h * ( max - min ) = ( x - min ) 
+						// => ( y - min ) / h * ( max - min ) + min = x
+
+			}
+
+			if ( allplotdata.global_extremes )
+			{
+				for ( var c in allplotdata )
+				{
+					if ( c == 'stats' || c == 'global_extremes' ) continue;
+					globmin = Math.min( allplotdata.stats[c].min, globmin );
+					globmax = Math.max( allplotdata.stats[c].max, globmax );
+				}
+
+				// normalize globmin/max:
+				console.log("global min: ", globmin, "max: ", globmax );
+				//globmin *= h/(globmax - globmin);
+				console.log("global min: ", globmin, "max: ", globmax );
+
+				allplotdata.stats['_GLOBAL_'] = { min:globmin, max:globmax };
+
+
+				if ( 1 )
+				for ( var c in allplotdata )
+				{
+					if ( c == 'stats' )
+						continue;
+					//console.log("global-normalize", allplotdata[c] );
+					for ( var i in allplotdata[c] )
+						//allplotdata[c][i][1] = normalize( denormalize( allplotdata[c][i][1], allplotdata.stats[c].min, allplotdata.stats[c].max, h ), globmin, globmax, h );
+						//allplotdata[c][i][1] = normalize( allplotdata[c][i][1], globmin, globmax, h );
+						allplotdata[c][i][1] = 
+							( 
+								//normalize( denormalize( allplotdata[c][i][1], allplotdata.stats[c].min, allplotdata.stats[c].max, 1 ), globmin, globmax, h )
+								//normalize( allplotdata[c][i][1] ,allplotdata.stats[c].min , allplotdata.stats[c].max, h )
+								normalize( allplotdata[c][i][1], globmin, globmax, h ) - globmin
+							)
+							// /(globmax-globmin);
+					//console.log("global-normalized", allplotdata[c] );
+				}
+			}
+
+			canvas.onmousemove = function(e) {
+				pos = e.target.getBoundingClientRect();
+				mousepos[0] = e.clientX - pos.left;
+				mousepos[1] = e.clientY - pos.top;
+				console.log("mousemove", canvas, arguments );
+				draw( canvas, current_currency );
+			};
+		} );
 	};
 
-	$.fn.plot = function(options) {
-		console.log("plot function", options, typeof options.selComp);
-	    return this.each(function() {
+	$.fn.plot.select = function(options) {
+		console.log( "**** select!");
+		return this.each(function() {
 			var $this = $(this);
+			console.log( "**** plot.select: ", this, $this, arguments );
 			var currency = options.currency;	// dataset key
 			var selComp = $(options.selComp)[0];	// mouseover element to select different dataset
 				// the $(..)[0] hopefully converts jQuery results and elements alike
@@ -35,89 +156,32 @@
 					selComp.style.backgroundColor='red';
 				}
 
-				draw(currency, options );
+				draw( this, currency, options );
 		} );
 	};
 
-	$.fn.plot.Constructor = Plot;
-
-	var canvas = document.getElementById('canvas');
-	canvas.style.position = 'relative'; // for offset
-	canvas.height = 400; // XXX parameterizable
-	canvas.width = Math.round( canvas.parentNode.clientWidth * 0.8 );
-	var ctx = canvas.getContext('2d');
-	var h = canvas.height;
-	var w = canvas.width;
-
-	var margin = 20;
-	var offs = margin/2; // x offset
-
-	allplotdata.stats = [];
-
-	var global_extremes = allplotdata.global_extremes == undefined ? true : allplotdata.global_extremes;
-
-	// normalize plotdata
-	for ( var c in allplotdata )
-	{
-		if ( c == 'stats' ) continue;
-		var min = 1<<30;
-		var max = 0;
-		for ( var i in allplotdata[c] )
-		{
-			min = Math.min( min, allplotdata[c][i][1] );
-			max = Math.max( max, allplotdata[c][i][1] );
-
-			allplotdata[c][i][2] = allplotdata[c][i][1]; // backup orig
-		}
-		console.log( "extremes for ", c, ": min=", min, " max=", max );
-		if ( ! global_extremes )
-			for ( var i in allplotdata[c] )
-				allplotdata[c][i][1] = min + ( allplotdata[c][i][1] - min ) / (max-min) * h;
-
-		allplotdata.stats[c] = { min:min, max:max };
-	}
-
-	var globmin=1000000, globmax=-1000000;
-	if ( global_extremes )
-	{
-		for ( var c in allplotdata )
-		{
-			if ( c == 'stats' ) continue;
-			console.log("min/max for ", c );
-			globmin = Math.min( allplotdata.stats[c].min, globmin );
-			globmax = Math.max( allplotdata.stats[c].max, globmax );
-		}
-		for ( var c in allplotdata )
-			for ( var i in allplotdata[c] )
-				allplotdata[c][i][1] = /*globmin +*/ ( allplotdata[c][i][1] - globmin ) / (globmax-globmin) * h;
-
-		// normalize globmin/max:
-		console.log("global min: ", globmin, "max: ", globmax );
-		globmin *= h/(globmax - globmin);
-		console.log("global min: ", globmin, "max: ", globmax );
-	}
 
 	var current_currency = null;
 	var current_options  = {};
 
-	var mousepos = [-1,-1];
-	canvas.onmousemove = function(e) {
-		pos = e.target.getBoundingClientRect();
-		mousepos[0] = e.clientX - pos.left;
-		mousepos[1] = e.clientY - pos.top;
-		draw( current_currency );
-	};
-
-	function draw( currency, options )
+	function draw( canvas, currency, options )
 	{
 		//options = options || current_options || {};
 		options = options || {};
 		current_currency = currency;
 		current_options  = options;
-		var plotdata = allplotdata[currency];
-		console.log("draw(",currency,")");
+		var plotdata = canvas.allplotdata[currency];
+		var stats = canvas.allplotdata.stats[currency];
+		var ctx = canvas.ctx;
+		var w = canvas.w;
+		var h = canvas.h;
+		var offs = canvas.offs;
+		console.log("draw", arguments, mousepos );
 		if (plotdata === undefined )
+		{
+			console.log("draw: no plot data");
 			return;
+		}
 		if ( options.clear != false )
 		ctx.clearRect(0,0,w,h);
 
@@ -133,10 +197,13 @@
 		ctx.moveTo(offs,  h-plotdata[0][1]);
 
 		for ( var i in plotdata )
-			ctx.lineTo( offs + hscale*i, h-plotdata[i][1] );
+			ctx.lineTo( offs + hscale*i, h - 
+			plotdata[i][1]
+		);
+		//			for ( var i in allplotdata[c] )
+		//				allplotdata[c][i][1] = normalize( denormalize( allplotdata[c][i][1], allplotdata.stats[c].min, allplotdata.stats[c].max, h ), globmin, globmax, h );
 		ctx.stroke();
 
-		var stats = allplotdata.stats[currency];
 
 		// mouse-y currency value, left
 		ctx.fillStyle='rgba(255,0,255,0.9)';
@@ -181,15 +248,17 @@
 		ctx.stroke();
 
 		// draw zero y 
-		if ( global_extremes && globmin < 0 )
+		if ( //plotdata.global_extremes &&
+			globmin < 0 )
 		{
 			ctx.strokeStyle ='rgba(128,128,128,0.5)';
 			ctx.lieWidth = 0.5;
 			ctx.beginPath();
-			ctx.moveTo( 0, h+globmin );
-			ctx.lineTo( w, h+globmin );
+			var gm = h*globmin / (globmax-globmin);
+			ctx.moveTo( 0, h+gm );
+			ctx.lineTo( w, h+gm );
 			ctx.stroke();
 		}
 
 	};
-}($);
+}(jQuery);
