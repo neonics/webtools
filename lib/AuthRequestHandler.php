1209e734b7298e61701f53f9376efd4fad97cdf7
kenney <kenney@neonics.com>
2015-04-17 00:40:41 +0200

    add .eot exception for authhandler (IE)

--- lib/AuthRequestHandler.php
+++ lib/AuthRequestHandler.php
@@ -5,7 +5,7 @@ class AuthRequestHandler extends RequestHandler
 {
 	protected function _handle( $request )
 	{
-		if ( !preg_match( "/\.(css|js|png|jpe?g|gif|woff2?|ttf|ico)$/", $request->requestFileURI ) )
+		if ( !preg_match( "/\.(css|js|png|jpe?g|gif|woff2?|ttf|eot|ico)$/", $request->requestFileURI ) )
 		{
 			ModuleManager::loadModule( "auth" );
 			if ( ! auth_user() )
