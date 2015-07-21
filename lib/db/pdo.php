8603b721b452e176393ff729e131ca9415c6f8f0
kenney <kenney@neonics.com>
2015-07-21 13:33:11 +0200

    lib/db/pdo.php: add execute*Query functions

--- lib/db/pdo.php
+++ lib/db/pdo.php
@@ -127,8 +127,199 @@ class PDODB extends PDO
 	}
 }
 
+
+/* Utility Functions */
+
+
 /**
  * produces comma separated string of count($array) question marks for use
  * in 'IN (...)' clauses.
  */
 function sql_q_list( $array ) { return substr( str_repeat( ",?", count( $array ) ), 1 ); }
+
+/**
+ * @returns last_insert_id
+ */
+function executeInsertQuery( $db, $table, $fields )
+{
+	static $sth = array();
+	static $query = null;
+
+	$sthn = $db->name . "_".$table."_insert:" . md5( implode(":", array_keys($fields) ) );
+
+	if ( ! array_key_exists( $sthn, $sth ) )
+	{
+		$query = "INSERT INTO $table ("
+			. implode( ",", array_keys( $fields ) )
+			. ") VALUES ("
+			. implode( ",", array_map( function(){ return "?"; }, $fields ) )
+			. ")"
+		. $db->returning();
+
+		#notice("<b>Query</b>: $query  (db: $db->driver)" );
+
+		if ( ( $sth[ $sthn ] = $db->prepare( $query ) ) === false )
+			throw new Exception( "error preparing query '$query': ". implode(':', $db->errorInfo() ) );
+	}
+
+	#debug(__FUNCTION__.": $query<pre>".print_r($fields,1)."</pre>" );
+
+	$result = null;
+
+	$arr = array_map( [$db, 'fix_sql_value'], array_values( $fields ) );
+
+	if ( $result = $sth[ $sthn ]->execute( $arr ) )
+	{
+		debug( __FUNCTION__, "ok" );
+	}
+	else
+	{
+		// in case PDOException isn't thrown
+		throw new Exception( "Error inserting into $table: ". implode(':', $sth[ $sthn ]->errorInfo() ) );
+	}
+
+	$lid = $db->last_insert_id( $sth[ $sthn ] );
+
+	$sth[ $sthn ]->closeCursor(); // xxx not closed on exception
+	return $lid;
+}
+
+
+function executeSelectQuery2( $db, $sql, $values = [] ) {
+	$sth = $db->prepare( $sql );
+	$values = array_map( [$db, 'fix_sql_value'], $values );
+	$sth->execute( $values );
+	$result = $sth->fetchAll( \PDO::FETCH_ASSOC );
+	$sth->closeCursor();
+	return $result;
+}
+
+
+function executeSelectQuery( $db, $table, $where = null, $extra = null )
+{
+	static $sth = array();
+
+	$sthn = $table."_select:" . ( is_null($where) ? "" : implode(':', array_keys( $where ) ) );
+#	debug( "(executeSelectQuery: $sthn)" );
+
+	if ( ! array_key_exists( $sthn, $sth ) )
+	{
+		$sth[ $sthn ] = $db->prepare( $q = "SELECT * FROM $table"
+			. ( is_null( $where ) || empty( $where )
+				? ""
+				: " WHERE " . implode( " AND ", array_map( function($i) { return "$i=?"; }, array_keys( $where ) ) )
+				)
+			. " $extra"	// order by, limit etc.
+		);
+#		debug( "sthn: $sthn; " . print_r( $sth[ $sthn ], true ) );
+	}
+
+	$result = null;
+	$values = is_null( $where ) ? [] : array_map( [$db, 'fix_sql_value'], array_values( $where ) );
+	if ( ( $result = $sth[ $sthn ]->execute( $values ) ) !== false )
+	{
+#		debug( "queried $table; numrows: ".$sth[ $sthn ]->rowCount() );
+		$result = $sth[ $sthn ]->fetchAll( PDO::FETCH_ASSOC );
+	}
+	else
+		throw new Exception( "Error querying $table: " . implode(':', $sth[ $sthn ]->errorInfo() ) );
+
+	$sth[ $sthn ]->closeCursor();
+
+	return $result;
+}
+
+function executeSelectQueryRequireSingle( $db, $table, $where, $extra = null ) {
+	$rows = executeSelectQuery( $db, $table, $where, $extra);
+
+	if ( $rows === false || $rows === null )
+		throw new Exception( "no rows for $table using where: " .
+			implode( " AND ", array_map( function($i) { return "$i=".$where[$i]; }, array_keys( $where ) ) )
+		);
+
+	if ( count($rows) != 1 )
+		throw new Exception( "expected 1 row for query on $table, got ".count( $rows ) );
+
+	return $rows[0];
+}
+
+
+/**
+ * @param $table table name
+ * @param $where hash
+ * @param $update hash
+ * @return num rows updated
+ */
+function executeUpdateQuery( $db, $table, $where, $update )
+{
+	static $sth = array();
+	#echo "<pre>".print_r(func_get_args(),1)."</pre>";
+
+	$sthn = $table."_update_" . md5( implode( '_', array_keys( $where ) ) . '_SET_' . implode('_', array_keys( $update ) ) );
+#	debug( "(executeUpdateQuery: $sthn)" );
+
+	if ( ! array_key_exists( $sthn, $sth ) )
+	{
+		$q=null;
+		$sth[ $sthn ] = $db->prepare( $q="UPDATE $table SET "
+			. implode( ", ", array_map( function($i) { return "$i=?"; }, array_keys( $update ) ) )
+			." WHERE "
+			. implode( " AND ", array_map( function($i) { return "$i=?"; }, array_keys( $where ) ) )
+		);
+	#	echo fold_debug(__FUNCTION__.": $q" );
+	#	debug( "sthn: $sthn; " . print_r( $sth[ $sthn ], true ) );
+	# debug ("prepared query: <b>$q</b>" );
+	}
+
+	$result = null;
+	$arr = array_map( [$db, 'fix_sql_value'],
+		array_merge( array_values( $update ), array_values( $where ) )
+	);
+
+	#echo "<pre>"; foreach ($arr as $k=>$v) { echo "$k=>$v: ". (is_null($v)?'NULL':'')."\n"; } echo "</pre>";
+	if ( $result = $sth[ $sthn ]->execute( $arr ) )
+		{}#echo "queried $table; numrows: ".$sth[ $sthn ]->rowCount(). "\n";
+	else
+		throw new Exception( "Error updating $table: " . implode(':', $sth[ $sthn ]->errorInfo() ) );
+
+
+	$r = $sth[ $sthn]->rowCount();
+
+	#debug("result: ".($result===false?"FALSE":$result) . " rowcount: $r");
+
+	$sth[ $sthn ]->closeCursor();
+	return $r;
+}
+
+
+function executeDeleteQuery( $db, $table, $where )
+{
+	static $sth = array();
+	if ( $where == null || ! is_array( $where ) || ! count( $where ) )
+		fatal("refusing delete without WHERE clause at ".__FILE__);
+
+	$sthn = $table."_delete:" . ( is_null($where) ? "" : implode(':', array_keys( $where ) ) );
+
+	if ( ! array_key_exists( $sthn, $sth ) )
+	{
+		$sth[ $sthn ] = $db->prepare( $q = "DELETE FROM $table WHERE "
+			. implode( " AND ", array_map( function($i) { return "$i=?"; }, array_keys( $where ) ) )
+		);
+		#echo "<pre>$q</pre>";
+	}
+
+	$result = null;
+	$arr = array_map( [$db, 'fix_sql_value'], array_values( $where ) );
+	if ( ( $result = $sth[ $sthn ]->execute( $arr ) ) !== false )
+	{
+		$result = $sth[ $sthn ]->rowCount();
+	}
+	else
+		throw new Exception( "Error querying $table: " . implode(':', $sth[ $sthn ]->errorInfo() ) );
+
+	$sth[ $sthn ]->closeCursor();
+
+	return $result;
+
+}
+
